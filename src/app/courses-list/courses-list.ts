a24a200cb1c16bca67320345736a6f7b7fb9787d
JoaoGabrielRLP <joojgaag@gmail.com>
2025-08-07 21:00:59 -0300

    Adicionado condições if no site

--- src/app/courses-list/courses-list.ts
+++ src/app/courses-list/courses-list.ts
@@ -10,9 +10,9 @@ import { Component, OnInit } from '@angular/core';
 export class CoursesList implements OnInit {
   title: string = "Cursos disponíveis"
   courses = [
-    {id: 1, title: 'Introdução a Angular', description: 'Aprenda Angular com esse curso', price: 49, date: '02-01-2025', soldOut: false, img: 'angular-logo.png'},
-    {id: 2, title: 'Angular avançado', description: 'Leve suas habilidades para o próximo nível', price: 100, date: '02-01-2025', soldOut: true, img: 'angular-logo.png'},
-    {id: 3, title: 'RxJS', description: 'Fluxo de dados assincronos', price: 45, date: '02-01-2025', soldOut: true, img: 'rxjs-1.svg'}
+    {id: 1, title: 'Introdução a Angular', description: 'Aprenda Angular com esse curso', price: 49, date: '02-01-2025', soldOut: false, img: 'angular-logo.png', onSale: false},
+    {id: 2, title: 'Angular avançado', description: 'Leve suas habilidades para o próximo nível', price: 100, date: '02-01-2025', soldOut: true, img: 'angular-logo.png', onSale: true},
+    {id: 3, title: 'RxJS', description: 'Fluxo de dados assincronos', price: 45, date: '02-01-2025', soldOut: true, img: 'rxjs-1.svg', onSale: true}
   ];
   /*Importando o ngOnInit*/
   ngOnInit(): void {
